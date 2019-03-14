78b69d6622d019700902f88dfbce9d3b8909d33c
Cloudswillopenforme <Christine.Tikhonowa@gmail.com>
2019-03-14 16:11:00 +0300

    Components and modules were added

---
+++ js/PhonesPage.js
@@ -0,0 +1,54 @@
+import PhonesList from "./components/PhonesList.js";
+import ShoppingCart from "./components/ShoppingCart.js";
+import Filter from "./components/Filter.js";
+import PhoneViewer from "./components/PhoneViewer.js";
+
+
+
+
+export default class PhonesPage {
+    constructor({element}) {
+        this._element = element;
+
+        this._render();
+
+        new PhonesList({
+            element: this._element.querySelector('[data-component="PhonesList"]'),
+        })
+        new ShoppingCart({
+            element: this._element.querySelector('[data-component="ShoppingCart"]'),
+        })
+        new Filter({
+            element: this._element.querySelector('[data-component="Filter"]'),
+        })
+        new PhoneViewer({
+            element: this._element.querySelector('[data-component="PhoneViewer"]'),
+        })
+    }
+
+    _render() {
+        this._element.innerHTML = `
+        
+            <div class="row">
+
+            <!--Sidebar-->
+            <div class="col-md-2">
+                <section>
+                    <div data-component="Filter"></div>
+                </section>
+
+                <section>
+                    <div data-component="ShoppingCart"></div>
+                </section>
+            </div>
+
+            <!--Main content-->
+            <div class="col-md-10">
+                <div data-component="PhonesList"></div>
+                <div data-component="PhoneViewer"></div>
+            </div>
+        </div>
+        
+        `;
+    }
+}
